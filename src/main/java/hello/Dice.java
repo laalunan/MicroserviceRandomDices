7241633ecbeb20947a974d9d1ed9eb80fe785b37
Lance Alunan <lance.alunan@gmail.com>
2018-08-30 11:19:32 +0800

    Initial Commit

---
+++ src/main/java/hello/Dice.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class Dice {
+
+	protected String name;
+	protected int number;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
---
+++ src/main/java/hello/RandomNumber.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomNumber {
+
+	@RequestMapping("/randomNumber")
+	public Map<String, List<Dice>> generateRandomDice() {
+
+		List<Dice> dice = new ArrayList<Dice>();
+		
+		Map<String, List<Dice>> map =new HashMap<String, List<Dice>>();
+		
+		for(int i = 1; i <= 3; i++) {
+			Random r = new Random();
+			Dice d = new Dice();
+			d.setName("Dice " + i);
+			d.setNumber(r.nextInt(6)+1);
+			dice.add(d);
+		}
+		
+		map.put("dice", dice);
+		
+		return map;
+	}
+}
